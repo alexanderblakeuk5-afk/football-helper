e461d3df1e501542c8c9b6e4bb8bb9b4c773a246
alexanderblakeuk5-afk <alexanderblake.uk5@gmail.com>
2026-06-09 03:35:44 +0500

    fix env loader

--- env_loader.py
+++ env_loader.py
@@ -1,5 +1,22 @@
 import os
+import json
 
-VK_USER_TOKEN = os.getenv("VK_USER_TOKEN")
-VK_GROUP_TOKEN = os.getenv("VK_GROUP_TOKEN")
-VK_USER_ID = os.getenv("VK_USER_ID")
+if os.path.exists("config.json"):
+
+    with open(
+        "config.json",
+        "r",
+        encoding="utf-8"
+    ) as f:
+
+        config = json.load(f)
+
+    VK_USER_TOKEN = config["vk_user_token"]
+    VK_GROUP_TOKEN = config["vk_group_token"]
+    VK_USER_ID = config["vk_user_id"]
+
+else:
+
+    VK_USER_TOKEN = os.getenv("VK_USER_TOKEN")
+    VK_GROUP_TOKEN = os.getenv("VK_GROUP_TOKEN")
+    VK_USER_ID = os.getenv("VK_USER_ID")
--- vk_sender.py
+++ vk_sender.py
@@ -1,27 +1,32 @@
 import requests
 import random
 
-from env_loader import VK_GROUP_TOKEN, VK_USER_ID
-
-params = {
-    "access_token": VK_GROUP_TOKEN,
-    "v": "5.199",
-    "peer_id": VK_USER_ID,
-    "message": message
-}
-
+from env_loader import (
+    VK_GROUP_TOKEN,
+    VK_USER_ID
+)
 
 def send_message(text):
 
     response = requests.post(
         "https://api.vk.com/method/messages.send",
         data={
-            "user_id": USER_ID,
+            "user_id": VK_USER_ID,
             "message": text,
-            "random_id": random.randint(1, 1000000000),
-            "access_token": TOKEN,
+            "random_id": random.randint(
+                1,
+                1000000000
+            ),
+            "access_token": VK_GROUP_TOKEN,
             "v": "5.199"
         }
     )
 
-    return response.json()
+    print(response.json())
+
+
+print("TOKEN =", VK_GROUP_TOKEN)
+print("USER_ID =", VK_USER_ID)
+
+if __name__ == "__main__":
+    send_message("Тест из vk_sender.py")
